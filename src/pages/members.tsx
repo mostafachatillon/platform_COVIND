7e6c44e7def1c7c01f29322ebea4d3d4bfcea6a0
emmanuelbouviere <mostafacebook@gmail.com>
2020-05-21 17:24:50 +0200

    Initial commit

---
+++ src/pages/members.tsx
@@ -0,0 +1,107 @@
+import React, { FC } from 'react';
+
+import {
+  Container,
+  Layout,
+  PageTitle,
+  PageBody,
+  Seo,
+} from '@components/shared';
+import { useSiteMetadata } from '@hooks';
+
+/** Displays the Terms and Conditions pages for the website. */
+const TermsPage: FC = () => {
+  const siteMetadata = useSiteMetadata();
+
+  return (
+    <Layout>
+      <Seo
+        title="Members"
+        description={`COVID-19 individual patient database`}
+        urlSlug="members/"
+      />
+
+      <Container>
+        <PageTitle>Members</PageTitle>
+
+        <PageBody>
+          <p>
+
+
+<iframe src="https://www.google.com/maps/d/u/0/embed?mid=18BjUsuguOLjEMLD0JoP3Dnwdqg248TTP" width="100%" height="300"></iframe>
+
+<ul>
+<li><strong>Assiut, Egypt</strong></li>
+</ul>
+<p><a href="http://www.aun.edu.eg/membercv.php?M_ID=2016">Aliae Hussein</a>, MD, Professor, Department of Chest Diseases, Faculty of Medicine, Assiut University</p>
+<ul>
+<li><strong>Brescia, Italy</strong></li>
+</ul>
+<p><a href="https://www.bresciamed.com/dott-ssa-elena-torri/">Elena Torri</a>, MD, BresciaMEd - Ambulatorio Medico ecografie e visite urgenza</p>
+
+<ul>
+<li><strong>Cambridge, Massachusetts, United States of America</strong></li>
+</ul>
+<p><a href="https://yaffelab.mit.edu/people/current">Christopher Barrett</a>, MD, Affiliate MIT Researcher, Beth Israel Deaconess Medical Center, Harvard Medical School  </p>
+<p><a href="https://yaffelab.mit.edu/people/pi">Michael B. Yaffe</a>, MD, Director of the Yaffe Lab, Professor of Biology and Biological Engineering, Koch Institute for Integrative Cancer Research, Center for Precision Cancer Medicine at MIT, Beth Israel Deaconess Medical Center</p>
+
+<ul>
+<li><strong>Cremona and Trieste, Italy</strong></li>
+</ul>
+<p><a href="https://www.centromedicoimetre60.it/daniele-generali/">Daniele Generali</a>, MD, Direttore di U.O. Multidisciplinare di Patologia Mammaria e Ricerca Traslazionale, Azienda Socio-Sanitaria Territoriale di Cremona, Professore Associato in Oncologia Medica, Dipartimento Universitario Clinico di Scienze Mediche, Chirurgiche e della Salute, Università degli Studi di Trieste</p>
+
+
+<ul>
+<li><strong>Denver, Colorado, United States of America</strong></li>
+</ul>
+<p><a href="https://www.denverhealth.org/provider-directory/m/moore-ernest-e">Ernest E. Moore</a>, MD, Ernest E. Moore Shock Trauma Center at Denver Health</p>
+<p><a href="https://scholar.google.com/citations?user=4uU5DugAAAAJ&amp;hl=en">Hunter Moore</a>, MD, Division of Transplant Surgery, Department of Surgery, University of Colorado</p>
+
+<ul>
+<li><strong>Lahore, Pakistan</strong></li>
+</ul>
+<p><a href="https://www.szmc.org.pk">Ahmad Imran</a>, MBBS, Shaikh Zayed Hospital</p>
+<p><a href="https://www.szmc.org.pk">Qurrat ul Ain Iqbal</a>, MD, Shaikh Zayed Hospital</p>
+<ul>
+<li><strong>London, United Kingdom</strong></li>
+</ul>
+<p><a href="https://www.nhs.uk/profiles/consultant/6130031">Emmanuel Ako</a>, MD, Department of Cardiology at Chelsea and Westminster NHS hospital</p>
+
+
+<ul>
+<li><strong>Moscow, Russia</strong></li>
+</ul>
+<p><a href="http://petrovax.com/medication/catalog/polyoxydonium/">Altana Mikhakhanova</a>, MD, NPO Petrovax Pharm LLC, a full-cycle biopharmaceutical company</p>
+
+
+<ul>
+<li><strong>Paris, France</strong></li>
+</ul>
+<p><a href="https://melwy.com/">Mostapha Benhenda</a>, PhD, Data scientist, Melwy</p>
+<ul>
+<li><strong>Rome, Italy</strong></li>
+</ul>
+<p><a href="https://www.policlinicogemelli.it/en/doctors/dott-danilo-buonsenso/">Danilo Buonsenso</a>, MD, U.O.C. di Pediatria, Fondazione Policlinico Universitario “A. Gemelli”</p>
+<ul>
+<li><strong>Tehran, Iran</strong></li>
+</ul>
+<p><a href="https://research.sbmu.ac.ir/webdocument/load.action?webdocument_code=8000&amp;masterCode=66035435">Seyed Sina Naghibi Irvani</a>, MD, MPH, MBA, Shahid Beheshti University of Medical Sciences</p>
+<ul>
+<li><strong>Tübingen, Germany</strong></li>
+</ul>
+<p><a href="https://www.medizin.uni-tuebingen.de/de/covid-19-studie">Diane Egger-Adam</a>, MD, Project Manager, Institute of Tropical Medicine, University of Tübingen</p>
+
+
+
+
+
+
+</p>
+
+        </PageBody>
+      </Container>
+    </Layout>
+  );
+};
+
+export default TermsPage;
